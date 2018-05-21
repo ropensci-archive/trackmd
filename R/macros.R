5c3b5e568741ff8d6f84b55ea4a86fbab46a4be3
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2018-05-21 13:27:12 -0700

    added substite and highlight

--- R/macros.R
+++ R/macros.R
@@ -6,7 +6,50 @@
 add <- function() {
   con <- rstudioapi::getActiveDocumentContext()
   docPos <- con$selection[[1]]$range$end
-  rstudioapi::insertText("{++  ++}", id = con$id)
-  docPosNew <- docPos + c(0, 4)
+  rstudioapi::insertText("{++++}", id = con$id)
+  docPosNew <- docPos + c(0, 3)
   rstudioapi::setCursorPosition(docPosNew, id = con$id)
 }
+
+
+#' Tracking a substitution
+#'
+#' Call this function as an addin to add the markdown track changes output for substitution.
+#'
+#' @export
+substitute <- function() {
+  con <- rstudioapi::getSourceEditorContext()
+  
+  # Get selected text
+  selection <- con$selection[[1]]$text
+  
+  # Add markup
+  docPos <- con$selection[[1]]$range$end
+  rstudioapi::insertText(paste0("{~~", selection, "~>~~}"), id = con$id)
+  
+  # Move cursor
+  docPosNew <- docPos + c(0, 5)
+  rstudioapi::setCursorPosition(docPosNew, id = con$id)
+}
+
+
+#' Tracking highlighting
+#'
+#' Call this function as an addin to add the markdown track changes output for highlighting
+#'
+#' @export
+highlight <- function() {
+  con <- rstudioapi::getSourceEditorContext()
+  
+  # Get selected text
+  selection <- con$selection[[1]]$text
+  
+  # Add markup
+  docPos <- con$selection[[1]]$range$end
+  rstudioapi::insertText(paste0("{==", selection, "==}{>><<}"), id = con$id)
+  
+  # Move cursor
+  docPosNew <- docPos + c(0, 9)
+  rstudioapi::setCursorPosition(docPosNew, id = con$id)
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# trackmd
-RStudio addin for tracking changes in Markdown format. Inspired by [Critic Markup](http://criticmarkup.com/).
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+[![Project Status: WIP - Initial development is in progress, but there
+has not yet been a stable, usable release suitable for the
+public.](http://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)
 
-[Original Issue](https://github.com/ropensci/unconf18/issues/76)
+trackmd
+=======
+
+RStudio addin for tracking {<sub><sub>changes</sub>&gt;new</sub>~} in
+Markdown format. Inspired by [Critic Markup](http://criticmarkup.com/).
+
+Created at the RopenSci unconference. [Original
+Issue](https://github.com/ropensci/unconf18/issues/76)
